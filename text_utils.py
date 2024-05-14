ddc0790b7a98dbe5d58c124b76ee14fd76542890
Chris-Nicki <christopher.nicki@gmail.com>
2024-05-13 22:32:02 -0600

    first commit

---
+++ text_utils.py
@@ -0,0 +1,16 @@
+def first_character_caps(string):
+    return string.title()
+
+def beginning_string_caps(string):    
+    return string.capitalize() 
+
+def make_it_all_lower(string):    
+    return string.lower()
+
+def fith_char(string):
+    new_string= string.strip()
+    return new_string[:5]
+
+
+
+
